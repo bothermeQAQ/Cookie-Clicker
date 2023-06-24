ee25ef54dcfbaf2398870f4d598f1991503536aa
Yonghao Wang <yonghaowang16@gmail.com>
2023-06-23 18:10:52 -0700

    first commit

---
+++ cookie-clicker/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+  <head>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    </link>
+     
+  <link href="https://fonts.googleapis.com/css2?family=Hanalei+Fill&display=swap" rel="stylesheet">
+  </link>
+</head>
